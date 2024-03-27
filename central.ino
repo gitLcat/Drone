16782cc394b52f3323ab88c2f2bd71f1b6eab91f
Michael Yang <135294530+gitLcat@users.noreply.github.com>
2024-03-26 23:36:41 -0700

    Update central.ino

--- central.ino
+++ central.ino
@@ -1,11 +1,14 @@
 #include <ArduinoBLE.h>
 
+int degreesX = 0;
+
+int degreesY = 0;
 
 void setup() {
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   while (!Serial);
 
@@ -105,11 +108,53 @@ void controlLed(BLEDevice peripheral) {
 
   // retrieve the LED characteristic
 
-  BLECharacteristic LEDCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
-
+  BLECharacteristic LEDCharacteristic = peripheral.characteristic("0001");
+  BLECharacteristic AccelerationCharacteristic = peripheral.characteristic("0002");
+  BLECharacteristic MagneticFieldCharacteristic = peripheral.characteristic("0003");
+  BLECharacteristic AltitudeCharacteristic = peripheral.characteristic("0004");
+  BLECharacteristic TemperatureCharacteristic = peripheral.characteristic("0005");
   if (!LEDCharacteristic) {
 
     Serial.println("Peripheral does not have LED characteristic!");
+
+    peripheral.disconnect();
+
+    return;
+
+  }
+if (!AccelerationCharacteristic) {
+
+    Serial.println("Peripheral does not have AccelerationCharacteristic!");
+
+    peripheral.disconnect();
+
+    return;
+
+  }
+
+  if (!MagneticFieldCharacteristic) {
+
+    Serial.println("Peripheral does not have MagneticFieldCharacteristic!");
+
+    peripheral.disconnect();
+
+    return;
+
+  }
+
+  if (!AltitudeCharacteristic) {
+
+    Serial.println("Peripheral does not have AltitudeCharacteristic!");
+
+    peripheral.disconnect();
+
+    return;
+
+  }
+
+  if (!TemperatureCharacteristic) {
+
+    Serial.println("Peripheral does not have TemperatureCharacteristic!");
 
     peripheral.disconnect();
 
@@ -121,27 +166,126 @@ void controlLed(BLEDevice peripheral) {
 
 
       
-        float oData[3];
-        LEDCharacteristic.readValue(oData,12);
+        float Gyro[3];
+        LEDCharacteristic.readValue(Gyro,12);
 
-        Serial.print("\t\tHeading: ");
-        Serial.print(oData[0]);
-        Serial.println('\t');
+        Serial.print("Gyro_X: ");
+        Serial.println(Gyro[0]);
 
-        Serial.print("\t\tPitch: ");
-        Serial.print(oData[1]);
-        Serial.println('\t');
 
-        Serial.print("\t\tRoll: ");
-        Serial.print(oData[2]);
-        Serial.println('\t');
-      
+        Serial.print("Gyro_Y: ");
+        Serial.println(Gyro[1]);
 
-      
 
-       
+        Serial.print("Gyro_Z: ");
+        Serial.println(Gyro[2]);
 
-    
+
+
+        float Accel[3];
+        AccelerationCharacteristic.readValue(Accel,12);
+
+        Serial.print("Accel_X: ");
+        Serial.print(Accel[0]);
+        Serial.println(' G');
+
+        Serial.print("Accel_Y: ");
+        Serial.print(Accel[1]);
+        Serial.println(' G');
+
+        Serial.print("Accel_Z: ");
+        Serial.print(Accel[2]);
+        Serial.println(' G');
+
+        if(Accel[0] > 0.1){
+
+    Accel[0] = 100*Accel[0];
+
+    degreesX = map(Accel[0], 0, 97, 0, 90);
+
+    Serial.print("Tilting up ");
+
+    Serial.print(degreesX);
+
+    Serial.println(" degrees");
+
+    }
+
+  if(Accel[0] < -0.1){
+
+    Accel[0] = 100*Accel[0];
+
+    degreesX = map(Accel[0], 0, -100, 0, 90);
+
+    Serial.print("Tilting down ");
+
+    Serial.print(degreesX);
+
+    Serial.println(" degrees");
+
+    }
+
+  if(Accel[1] > 0.1){
+
+    Accel[1] = 100*Accel[1];
+
+    degreesY = map(Accel[1], 0, 97, 0, 90);
+
+    Serial.print("Tilting left ");
+
+    Serial.print(degreesY);
+
+    Serial.println(" degrees");
+
+    }
+
+  if(Accel[1] < -0.1){
+
+    Accel[1] = 100*Accel[1];
+
+    degreesY = map(Accel[1], 0, -100, 0, 90);
+
+    Serial.print("Tilting right ");
+
+    Serial.print(degreesY);
+
+    Serial.println(" degrees");
+
+    }
+
+        float Magn[3];
+        MagneticFieldCharacteristic.readValue(Magn,12);
+        Serial.print("Magn_X: ");
+        Serial.println(Magn[0]);
+
+        Serial.print("Magn_Y: ");
+        Serial.println(Magn[1]);
+
+
+        Serial.print("Magn_Z: ");
+        Serial.println(Magn[2]);
+        
+
+        float Alti[2];
+        AltitudeCharacteristic.readValue(Alti,12);
+        Serial.print("Pressure : ");
+        Serial.print(Alti[0]);
+        Serial.println('kPa');
+
+        Serial.print("altitude : ");
+        Serial.println(Alti[1]);
+        
+        float Temp[2];
+        TemperatureCharacteristic.readValue(Temp,12);
+        Serial.print("Temperature :");
+        Serial.print(Temp[0]);
+        Serial.println(" °C");
+
+        Serial.print("Humidity :");
+        Serial.print(Temp[1]);
+        Serial.println(" %");
+        Serial.println();
+        Serial.println();
     delay(500);
 
   }
