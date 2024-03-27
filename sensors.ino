9108336ded4dc8d5e3aa7c96aee17e9162247778
Michael Yang <135294530+gitLcat@users.noreply.github.com>
2024-03-26 23:36:54 -0700

    Update sensors.ino

--- sensors.ino
+++ sensors.ino
@@ -1,22 +1,54 @@
 #include <ArduinoBLE.h>
-
-
+#include "Arduino_BMI270_BMM150.h"
+#include "Arduino_LPS22HB.h"
+#include "Arduino_HS300x.h"
 
 BLEService LEDService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
 
 // BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
 
-BLECharacteristic LEDCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify,12);
-
+BLECharacteristic LEDCharacteristic("0001", BLERead | BLENotify | BLEBroadcast,12);
+BLECharacteristic AccelerationCharacteristic("0002", BLERead | BLENotify | BLEBroadcast,12);
+BLECharacteristic MagneticFieldCharacteristic("0003", BLERead | BLENotify | BLEBroadcast,12);
+BLECharacteristic AltitudeCharacteristic("0004", BLERead | BLENotify | BLEBroadcast,12);
+BLECharacteristic TemperatureCharacteristic("0005", BLERead | BLENotify | BLEBroadcast,12);
+float x,y,z,q,w,e,r,t,u,i,o,p,a;
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // begin initialization
+while (!Serial);
 
+  Serial.println("Started");
+  
   if (!BLE.begin()) {
 
     Serial.println("starting Bluetooth® Low Energy failed!");
+
+  }
+
+if (!IMU.begin()) {
+
+    Serial.println("Failed to initialize IMU!");
+
+    while (1);
+
+  }
+
+if (!BARO.begin()) {
+
+    Serial.println("Failed to initialize pressure sensor!");
+
+    while (1);
+
+  }
+
+  if (!HS300x.begin()) {
+
+    Serial.println("Failed to initialize humidity temperature sensor!");
+
+    while (1);
 
   }
 
@@ -29,7 +61,10 @@ void setup() {
   // add the characteristic to the service
 
   LEDService.addCharacteristic(LEDCharacteristic);
-
+  LEDService.addCharacteristic(AccelerationCharacteristic);
+  LEDService.addCharacteristic(MagneticFieldCharacteristic);
+  LEDService.addCharacteristic(AltitudeCharacteristic);
+  LEDService.addCharacteristic(TemperatureCharacteristic);
   // add service
 
   BLE.addService(LEDService);
@@ -61,12 +96,33 @@ void loop() {
     // while the central is still connected to peripheral:
 
     while (central.connected()) {
+      IMU.readGyroscope(x, y, z);
       float Gyro[3];
-      Gyro[0] = -10;
-      Gyro[1] = -10;
-      Gyro[2] = -10;
-      
+      Gyro[0] = x;
+      Gyro[1] = y;
+      Gyro[2] = z;
+      IMU.readAcceleration(q, w, e);
+      float Accel[3];
+      Accel[0]= q;
+      Accel[1]= w;
+      Accel[2]= e;
+      IMU.readMagneticField(r, t, u);
+      float Magn[3];
+      Magn[0]= r;
+      Magn[1]= t;
+      Magn[2]= u;
+      float Alti[2];
+      Alti[0] = BARO.readPressure();
+      Alti[1] = 44330 * ( 1 - pow(Alti[0]/101.325, 1/5.255) );
+      float Temp[2];
+      Temp[0] = HS300x.readTemperature();
+      Temp[1] = HS300x.readHumidity();
     LEDCharacteristic.writeValue((byte*) &Gyro,12);
+    AccelerationCharacteristic.writeValue((byte*) &Accel,12);
+    MagneticFieldCharacteristic.writeValue((byte*) &Magn,12);
+    AltitudeCharacteristic.writeValue((byte*) &Alti,12);
+    TemperatureCharacteristic.writeValue((byte*) &Temp,12);
+    delay(500);
     }
 
     // when the central disconnects, print it out:
@@ -78,3 +134,4 @@ void loop() {
   }
 
 }
+
