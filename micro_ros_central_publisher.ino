1f073414abf33e6ad241c3173763296ee9ac8740
Michael Yang <135294530+gitLcat@users.noreply.github.com>
2024-04-29 02:57:42 -0700

    Update micro_ros_central_publisher.ino

--- micro_ros_central_publisher.ino
+++ micro_ros_central_publisher.ino
@@ -47,7 +47,7 @@ void setup() {
   // create publisher
 
 
-  RCCHECK(rclc_publisher_init_best_effort(
+  RCCHECK(rclc_publisher_init_default(
     &sensor_data,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
